06d577a9618ce871b9b44080a44bd9a31b88d338
Faizan Ullah <faizanullah1999@gmail.com>
2020-07-15 23:08:16 +0500

    Footer almost complete. Brands, Banner Ads, New phones, and Latest News was added. Owl Carousel was used again and its js file was updated again.

--- index.js
+++ index.js
@@ -36,5 +36,38 @@ $(document).ready(function(){
         var filterValue=$(this).attr('data-filter');
         $grid.isotope({filter:filterValue});
     })
+
+        //New Phones Owl Carousel
+        $("#new-phones .owl-carousel").owlCarousel({
+            loop:true,
+            nav:false,
+            dots:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:5
+                }
+            }
+        });
+
+        //News Owl Carousel
+        $("#news .owl-carousel").owlCarousel({
+            loop:true,
+            nav:false,
+            dots:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                }
+            }
+        })
     
 });
