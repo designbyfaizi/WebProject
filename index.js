381c55f46a03d6d8e27ec89f71bd7e9d070df3dd
Faizan Ullah <faizanullah1999@gmail.com>
2020-07-15 16:59:04 +0500

    Added the Top Sale Area and started the Brand Section

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ $(document).ready(function(){
     $("#banner-area .owl-carousel").owlCarousel({
         dots:true,
         items:1
+    });
 
+    //Top Sale Owl Carousel
+    $("#top-sale .owl-carousel").owlCarousel({
+        loop:true,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
     });
 });
